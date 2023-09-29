e65153542678708f9a25ff8f82a2d62027666eb0
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-09-29 22:38:25 +0600

    50 done

--- module-50/email-authentication/src/components/Login/Login.jsx
+++ module-50/email-authentication/src/components/Login/Login.jsx
@@ -1,22 +1,64 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
+    const [registerError, setRegisterError] = useState('');
+    const [success, setSuccess] = useState('');
+    const emailRef = useRef(null)
 
 
-    const handleLogIn = e =>{
+    const handleLogIn = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email,password);
+        console.log(email, password);
+
+        // reset error and success
+        setRegisterError('')
+        setSuccess('')
+
+
 
         // add validation 
         signInWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            console.log(result.user)
+            .then(result => {
+                console.log(result.user)
+                if (result.user.emailVerified) {
+                    setSuccess("User logged successfully")
+                }
+                else{
+                    alert('please verify your email address')
+                }
+            })
+            .catch(error => {
+                console.log(error)
+                setRegisterError(error.message)
+            })
+    }
+
+
+    const handleForgetPassword = () => {
+        const email = emailRef.current.value;
+        if(!email) {
+            console.log('please provid a mail', emailRef.current.value)
+            return;
+        }else if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)){
+            console.log('please write a valid email');
+            return;
+        }
+
+
+        // send validation email
+        sendPasswordResetEmail(auth, email)
+        .then(() =>{
+            alert('please check your email')
         })
-        .catch(error => console.log(error))
+        .catch(error =>{
+            console.log(error)
+        })
     }
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -32,21 +74,32 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" name="email" className="input input-bordered" />
+                                <input required type="email"
+                                 placeholder="email" 
+                                 name="email"
+                                 ref={emailRef}
+                                 className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                                <input required type="password" name="password" placeholder="password" className="input input-bordered" />
                                 <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    <a onClick={handleForgetPassword} href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
                             </div>
                         </form>
+                        {
+                            registerError && <p className="text-red-600">{registerError}</p>
+                        }
+                        {
+                            success && <p className="text-green-700">{success}</p>
+                        }
+                        <p>New to the website? Please <Link className="text-purple-600" to="/register">Register</Link></p>
                     </div>
                 </div>
             </div>
--- module-50/email-authentication/src/components/Register/Register.jsx
+++ module-50/email-authentication/src/components/Register/Register.jsx
@@ -1,7 +1,8 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 const Register = () => {
     const [registerError, setRegisterError] = useState('');
@@ -10,15 +11,17 @@ const Register = () => {
 
     const handleRegister = e => {
         e.preventDefault();
+        const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         const accepted = e.target.terms.checked
 
         console.log(email, password, accepted);
 
-        // reset error
+        // reset error and success
         setRegisterError('')
         setSuccess('')
+        
 
         if (password.length < 6) {
             setRegisterError("password should be at least 6 characters or longer");
@@ -37,6 +40,22 @@ const Register = () => {
             .then(result => {
                 console.log(result.user)
                 setSuccess("User created successfully")
+
+                // Update Profile
+
+                updateProfile(result.user,{
+                    displayName: name,
+                    photoURL: ""
+                })
+
+                .then(()=> console.log('profile updated'))
+
+                // send verification email
+
+                sendEmailVerification(result.user)
+                .then(() =>{
+                    alert('please check email and verify your account')
+                })
             })
             .catch(error => {
                 console.log(error)
@@ -48,6 +67,7 @@ const Register = () => {
             <div className="mx-auto md: w-1/2">
                 <p className="text-3xl mb-5">Please Register</p>
                 <form onSubmit={handleRegister}>
+                    <input required placeholder="Input name" className="mb-4 w-full py-2 px-4 rounded" type="text" name="name" id="" /><br />
                     <input required placeholder="Email Address" className="mb-4 w-full py-2 px-4 rounded" type="email" name="email" id="" /><br />
                     <div className="mb-4 relative">
                         <input required placeholder="Input Password" className="w-full py-2 px-4 rounded" type={showPassword ? "text" : "password"} name="password" id="" />
@@ -71,6 +91,7 @@ const Register = () => {
                 {
                     success && <p className="text-green-700">{success}</p>
                 }
+                <p>Already have an account? Please <Link className="text-violet-600" to="/login">Login</Link></p>
             </div>
         </div>
     );
